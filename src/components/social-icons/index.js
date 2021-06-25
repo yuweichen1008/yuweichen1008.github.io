3362ecc120b660c395c0e00451b9ef26abad8f6e
yuweichen1008 <yuweiichen@gmail.com>
2021-06-25 20:32:30 +0800

    remove redundent

--- src/components/social-icons/index.js
+++ src/components/social-icons/index.js
@@ -29,9 +29,9 @@ const SocialIcon = ({ kind, href, size = 8 }) => {
       href={href}
     >
       <span className="sr-only">{kind}</span>
-      <SocialSvg
+      {/* <SocialSvg
         className={`fill-current text-gray-700 dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 h-${size} w-${size}`}
-      />
+      /> */}
     </a>
   )
 }
